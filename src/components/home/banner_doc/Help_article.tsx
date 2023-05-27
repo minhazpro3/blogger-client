fb678df75828acd16b9b53bca4f98e34fe5e8e0b
minhazpro3 <minhazpro1@gmail.com>
2023-05-28 01:44:34 +0600

    help article center

--- src/components/home/banner_doc/Help_article.tsx
+++ src/components/home/banner_doc/Help_article.tsx
@@ -1,10 +1,10 @@
 export default function Help_article(){
 
     return(
-        <div>
-            <h2 className="text-2xl font-medium ">Getting started with the API</h2>
+        <div className="bg-slate-950 shadow-base rounded-md drop-shadow-lg shadow-indigo-700 px-5 sm:px-3 md:px-5 lg:px-6 py-5 sm:py-4 md:py-5 lg:py-6 ">
+            <h2 className="text-2xl font-medium text-white">Getting started with the API</h2>
             <p className="text-base text-white my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolore odio reiciendis hic eum numquam laudantium quo asperiores repellendus natus.</p>
-            <button className="text-blue-800 bg-white px-6 py-2 ">Read more &#8594;</button>
+            <button className="text-blue-800  hover:translate-x-1 duration-75 ease-in  px-6 py-2 my-2 ">Read more &#8594;</button>
         </div>
     )
 }
--- src/components/home/banner_doc/Help_articles.tsx
+++ src/components/home/banner_doc/Help_articles.tsx
@@ -4,7 +4,7 @@ export default function Help_articles() {
 
     return (
         <div className="container mx-auto px-8 md:px-0">
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 justify-items-stretch  gap-6 sm:gap-7 md:gap-6 lg:gap-8 text-center my-12">
                 <Help_article />
                 <Help_article />
                 <Help_article />
