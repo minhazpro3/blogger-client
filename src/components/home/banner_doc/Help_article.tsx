3d74d293026acb5e4565ff758a3715ac75e98917
minhazpro3 <minhazpro1@gmail.com>
2023-06-01 21:55:35 +0600

    article section styled

--- src/components/home/banner_doc/Help_article.tsx
+++ src/components/home/banner_doc/Help_article.tsx
@@ -8,10 +8,14 @@ export default function Help_article() {
         AOS.init();
       }, [])
     return (
-        <div data-aos="fade-up" data-aos-duration="2000" className="bg-slate-950 shadow-base rounded-md drop-shadow-lg shadow-indigo-700 px-5 sm:px-3 md:px-5 lg:px-6 py-6 sm:py-6 md:py-6 lg:py-7 ">
+        <div data-aos="fade-up" data-aos-duration="2000" className="h-36 w-full rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1 my-4">
+
+        <div  className="bg-slate-950 shadow-base rounded-md drop-shadow-lg shadow-indigo-700 px-5 sm:px-3 md:px-5 lg:px-6 py-6 sm:py-6 md:py-6 lg:py-7 
+        lg:h-[260px] md:h-[300px] sm:h-[200px] h-350px">
             <h2 className="text-2xl font-medium text-white  pt-6">Getting started with the API</h2>
             <p className="text-base text-white my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolore odio reiciendis hic eum numquam laudantium quo asperiores repellendus natus.</p>
             <button className="text-blue-800  hover:translate-x-1 duration-75 ease-in  px-6 py-2 my-2 ">Read more &#8594;</button>
+        </div>
         </div>
     )
 }
