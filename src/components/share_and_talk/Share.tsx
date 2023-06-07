270b630613c9eac2d8b32ef19e3bd5c6860b16da
minhazpro3 <minhazpro1@gmail.com>
2023-06-07 22:51:38 +0600

    added find , approached section

--- src/components/share_and_talk/Share.tsx
+++ src/components/share_and_talk/Share.tsx
@@ -24,8 +24,8 @@ const Share = () => {
                 <div className="my-12">
                     <div className="grid grid-cols-3 gap-4">
                         {/* messaging */}
-                        <div className="col-span-1 border-2 relative border-slate-800 sm:h-[450px] md:h-[600px] py-8 px-6">
-                            
+                        <div className="col-span-1 border-2 relative border-slate-800 sm:h-[450px] rounded-lg md:h-[600px] py-8 px-6">
+
                             <div className="flex justify-center">
                                 <lottie-player
                                     id="firstLottie"
@@ -39,7 +39,7 @@ const Share = () => {
                                 ></lottie-player>
                             </div>
                             <h2 className="text-white absolute bottom-8 left-6 text-xl sm:text-3xl">Instant Talk</h2>
-                            <div className="absolute bottom-7 right-6"> 
+                            <div className="absolute bottom-5 right-6">
                                 <lottie-player
                                     id="firstLottie"
                                     ref={ref}
@@ -51,14 +51,67 @@ const Share = () => {
                                 ></lottie-player></div>
                         </div>
                         {/* points approached etc */}
-                        <div className="  text-white">
+                        <div className=" col-span-2 text-white  ">
                             <div>
-                                <div>
-                                    <div>
-                                        Fine details
+                                <div className="sm:flex justify-center w-full gap-4">
+                                    {/* find details */}
+                                    <div className="w-full ">
+                                        <div className="border-4 relative h-[290px] p-4
+                                   border-slate-800 rounded-lg">
+                                          <h4 className="text-base text-green-300 font-medium my-2 px-4">MAGIC POT</h4>
+                                            <h2 className="text-4xl font-medium text-white px-4 my-7">Fine <br /> Details</h2>
+                                            <p className="px-4 absolute bottom-2 py-2">Feature cards section <br />concept details by Yasir Arthure</p>
+                                            <div className="absolute top-3 right-4 ">
+                                                <lottie-player
+                                                    id="firstLottie"
+                                                    ref={ref}
+                                                    autoplay
+                                                    loop
+                                                    mode="normal"
+                                                    src="https://assets7.lottiefiles.com/private_files/lf30_LOw4AL.json"
+                                                    style={{ width: "150px", height: "150px" }}
+                                                ></lottie-player></div>
+                                            <div className="absolute bottom-2 right-4 ">
+                                                <lottie-player
+                                                    id="firstLottie"
+                                                    ref={ref}
+                                                    autoplay
+                                                    loop
+                                                    mode="normal"
+                                                    src="https://assets10.lottiefiles.com/packages/lf20_80nu1g6c.json"
+                                                    style={{ width: "70px", height: "70px" }}
+                                                ></lottie-player></div>
+                                        </div>
                                     </div>
-                                    <div>
-                                        Encrypted Approached
+                                    {/* approached */}
+                                    <div className="w-full ">
+                                        <div className="border-4 relative h-[290px] p-4
+                                   border-slate-800 rounded-lg">
+                                           
+                                            <h4 className="text-base text-green-300 font-medium my-2 px-4">TOP NOTCH SHORT</h4>
+                                            <h2 className="text-4xl font-medium text-white px-4 my-7">Encrypted <br /> Approached</h2>
+                                            <p className="px-4 absolute bottom-2 py-2">Feature cards section <br />concept details by Yasir Arthure</p>
+                                            <div className="absolute top-3 right-4 ">
+                                                <lottie-player
+                                                    id="firstLottie"
+                                                    ref={ref}
+                                                    autoplay
+                                                    loop
+                                                    mode="normal"
+                                                    src="https://assets8.lottiefiles.com/packages/lf20_ibmd3e7f.json"
+                                                    style={{ width: "150px", height: "150px" }}
+                                                ></lottie-player></div>
+                                            <div className="absolute bottom-2 right-4 ">
+                                                <lottie-player
+                                                    id="firstLottie"
+                                                    ref={ref}
+                                                    autoplay
+                                                    loop
+                                                    mode="normal"
+                                                    src="https://assets10.lottiefiles.com/packages/lf20_80nu1g6c.json"
+                                                    style={{ width: "70px", height: "70px" }}
+                                                ></lottie-player></div>
+                                        </div>
                                     </div>
                                 </div>
                                 <div>
