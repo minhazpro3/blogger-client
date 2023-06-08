f0bbda70075168cc0334e2292f059c42ffc88310
minhazpro3 <minhazpro1@gmail.com>
2023-06-09 01:29:46 +0600

    added a sidebar

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,3 +1,4 @@
+import Title_sidebar from "@/components/docs/Title_sidebar";
 
 
 export default function DocsLayout({ children }: { children: React.ReactNode }) {
@@ -6,8 +7,8 @@ export default function DocsLayout({ children }: { children: React.ReactNode }) 
         <div className="">
             <div className="grid grid-cols-12 text-white gap-4 px-8">
                 {/*   sidebar */}
-                <div className="col-span-3 ">
-                    col span 3
+                <div className="col-span-3 bg-gradient-to-r from-indigo-700  to-indigo-900 inline px-6">
+                    <Title_sidebar/>
                 </div>
                 {/* main  */}
                 <div className="col-span-6">
---
+++ src/components/docs/Title_sidebar.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { HiDocumentText } from "react-icons/hi";
+
+const Title_sidebar = () => {
+    return (
+        <div className='my-8'>
+
+           <div className='grid grid-rows-4 gap-2'>
+           <ul className='text-white flex gap-4 items-center justify-center bg-indigo-950 px-10 py-3 rounded-lg shadow-md '> 
+            <span className='text-xl'>
+                <HiDocumentText />
+                </span> 
+                <h4 className='font-medium text-xl'>Documentation</h4>
+                </ul>
+           <ul className='text-white flex gap-4 items-center justify-center bg-indigo-950 px-10 py-3 rounded-lg shadow-md '> 
+            <span className='text-xl'>
+                <HiDocumentText />
+                </span> 
+                <h4 className='font-medium text-xl'>Documentation</h4>
+                </ul>
+           <ul className='text-white flex gap-4 items-center justify-center bg-indigo-950 px-10 py-3 rounded-lg shadow-md '> 
+            <span className='text-xl'>
+                <HiDocumentText />
+                </span> 
+                <h4 className='font-medium text-xl'>Documentation</h4>
+                </ul>
+           <ul className='text-white flex gap-4 items-center justify-center bg-indigo-950 px-10 py-3 rounded-lg shadow-md '> 
+            <span className='text-xl'>
+                <HiDocumentText />
+                </span> 
+                <h4 className='font-medium text-xl'>Documentation</h4>
+                </ul>
+           <ul className='text-white flex gap-4 items-center justify-center bg-indigo-950 px-10 py-3 rounded-lg shadow-md '> 
+            <span className='text-xl'>
+                <HiDocumentText />
+                </span> 
+                <h4 className='font-medium text-xl'>Documentation</h4>
+                </ul>
+           <ul className='text-white flex gap-4 items-center justify-center bg-indigo-950 px-10 py-3 rounded-lg shadow-md '> 
+            <span className='text-xl'>
+                <HiDocumentText />
+                </span> 
+                <h4 className='font-medium text-xl'>Documentation</h4>
+                </ul>
+           </div>
+        </div>
+    )
+}
+
+export default Title_sidebar
