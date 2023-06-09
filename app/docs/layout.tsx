d4e928ad5460fc89c421450adb6466349d267c04
minhazpro3 <minhazpro1@gmail.com>
2023-06-09 14:58:19 +0600

    design sidebar

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -4,14 +4,14 @@ import Title_sidebar from "@/components/docs/Title_sidebar";
 export default function DocsLayout({ children }: { children: React.ReactNode }) {
 
     return (
-        <div className="">
+        <div className="my-5">
             <div className="grid grid-cols-12 text-white gap-4 px-8">
                 {/*   sidebar */}
-                <div className="col-span-3 bg-gradient-to-r from-indigo-700  to-indigo-900 inline px-6">
+                <div className="col-span-3 bg-indigo-950 rounded-lg  px-6">
                     <Title_sidebar/>
                 </div>
                 {/* main  */}
-                <div className="col-span-6">
+                <div className="col-span-6 static">
                     {children}
                 </div>
                 {/* documents */}
--- src/components/docs/Title_sidebar.tsx
+++ src/components/docs/Title_sidebar.tsx
@@ -1,48 +1,35 @@
 import React from 'react'
 import { HiDocumentText } from "react-icons/hi";
+import { IoIosHome } from "react-icons/io";
+import { AiTwotoneProject } from "react-icons/ai";
 
 const Title_sidebar = () => {
     return (
         <div className='my-8'>
-
-           <div className='grid grid-rows-4 gap-2'>
-           <ul className='text-white flex gap-4 items-center justify-center bg-indigo-950 px-10 py-3 rounded-lg shadow-md '> 
-            <span className='text-xl'>
-                <HiDocumentText />
-                </span> 
-                <h4 className='font-medium text-xl'>Documentation</h4>
+            <h2 className='text-2xl'>Wave Doc !</h2>
+           <div className='my-4'> 
+            <h3 className='text-sm my-2 uppercase'>Dashboard</h3>
+            <div className='grid grid-rows-4 gap-2 justify-start'>
+                <ul className='text-white flex gap-2 justify-start hover:bg-gray-700 active:bg-gray-700 bg-indigo-950   w-50 px-8  hover:cursor-pointer py-2 rounded-lg shadow-md '>
+                    <span className='text-xl'>
+                        <IoIosHome />
+                    </span>
+                    <h4 className='font-medium text-base'>Overview</h4>
                 </ul>
-           <ul className='text-white flex gap-4 items-center justify-center bg-indigo-950 px-10 py-3 rounded-lg shadow-md '> 
-            <span className='text-xl'>
-                <HiDocumentText />
-                </span> 
-                <h4 className='font-medium text-xl'>Documentation</h4>
+                <ul className='text-white flex gap-2 justify-start hover:bg-gray-700 active:bg-gray-700 bg-indigo-950   w-50 px-8  hover:cursor-pointer py-2 rounded-lg shadow-md '>
+                    <span className='text-xl'>
+                        <HiDocumentText />
+                    </span>
+                    <h4 className='font-medium text-base'>Documentation</h4>
                 </ul>
-           <ul className='text-white flex gap-4 items-center justify-center bg-indigo-950 px-10 py-3 rounded-lg shadow-md '> 
-            <span className='text-xl'>
-                <HiDocumentText />
-                </span> 
-                <h4 className='font-medium text-xl'>Documentation</h4>
+                <ul className='text-white flex gap-2 justify-start hover:bg-gray-700 active:bg-gray-700 bg-indigo-950   w-50 px-8  hover:cursor-pointer py-2 rounded-lg shadow-md '>
+                    <span className='text-xl'>
+                        <AiTwotoneProject />
+                    </span>
+                    <h4 className='font-medium text-base'>Current Projects <span className='ml-4'>10</span></h4>
                 </ul>
-           <ul className='text-white flex gap-4 items-center justify-center bg-indigo-950 px-10 py-3 rounded-lg shadow-md '> 
-            <span className='text-xl'>
-                <HiDocumentText />
-                </span> 
-                <h4 className='font-medium text-xl'>Documentation</h4>
-                </ul>
-           <ul className='text-white flex gap-4 items-center justify-center bg-indigo-950 px-10 py-3 rounded-lg shadow-md '> 
-            <span className='text-xl'>
-                <HiDocumentText />
-                </span> 
-                <h4 className='font-medium text-xl'>Documentation</h4>
-                </ul>
-           <ul className='text-white flex gap-4 items-center justify-center bg-indigo-950 px-10 py-3 rounded-lg shadow-md '> 
-            <span className='text-xl'>
-                <HiDocumentText />
-                </span> 
-                <h4 className='font-medium text-xl'>Documentation</h4>
-                </ul>
-           </div>
+            </div>
+            </div>
         </div>
     )
 }
