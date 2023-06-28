463b07bfa05accfaad46101ce1765ab4a1e7f27d
codergalib20 <coderboygalif@gmail.com>
2023-06-28 21:22:59 +0600

    install package for react markdown (import rehypeRaw from rehype-raw;)

--- src/lib/markdownPreview.tsx
+++ src/lib/markdownPreview.tsx
@@ -12,10 +12,13 @@ export default function MarkdownPreview({
   return (
     <div className="markdown-preview-custom">
       <ReactMarkdown
-        remarkPlugins={[remarkGfm, rehypeRaw]}
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
         components={{
           code({ node, inline, className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || "");
+            console.log({ match });
+            console.log({ className });
             return !inline && match ? (
               <SyntaxHighlighter
                 {...props}
@@ -32,7 +35,6 @@ export default function MarkdownPreview({
             );
           },
           // Map `h1` (`# heading`) to use `h2`s.
-          h1: "h2",
           // Rewrite `em`s (`*like so*`) to `i` with a red foreground color.
           em: ({ node, ...props }) => <i style={{ color: "red" }} {...props} />,
         }}
