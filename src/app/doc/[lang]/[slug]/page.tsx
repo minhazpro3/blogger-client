74a7db7d8a9350cd9df423191c2ea42f7feebaa1
codergalib20 <coderboygalif@gmail.com>
2023-06-28 20:07:16 +0600

    added axios for get data

--- src/app/doc/[lang]/[slug]/page.tsx
+++ src/app/doc/[lang]/[slug]/page.tsx
@@ -1,9 +1,21 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 "use client";
+import axios from "@/lib/axios";
 import { useRouter } from "next/navigation";
+import { NextPage } from "next/types";
+interface ParamsTypes {
+  slug: string;
+  lang: string;
+}
 
-const Page = ({ params }: any) => {
+async function getData({ slug }: any) {
+  const res = await axios.get(`/api/v1/doc/slug/${slug as string}`);
+  return (res as any).json();
+}
+
+const Page = async ({ params }: ParamsTypes) => {
   const router = useRouter();
+  const data = await getData({ slug: params.slug });
+  console.log(data);
   return <div>Welcome to slug {params.slug}</div>;
 };
 
