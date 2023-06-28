29c0f537f79d0854b6159ad5231b290fe4de6287
codergalib20 <coderboygalif@gmail.com>
2023-06-28 20:54:28 +0600

    install package for react markdown (import rehypeRaw from rehype-raw;)

--- src/lib/markdownPreview.tsx
+++ src/lib/markdownPreview.tsx
@@ -1,4 +1,9 @@
+import rehypeRaw from "rehype-raw";
+import remarkGfm from "remark-gfm";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
 export default function MarkdownPreview({
   markdownContent,
 }: {
@@ -6,7 +11,34 @@ export default function MarkdownPreview({
 }) {
   return (
     <div className="markdown-preview-custom">
-      <ReactMarkdown>{markdownContent}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm, rehypeRaw]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                // eslint-disable-next-line react/no-children-prop
+                children={String(children).replace(/\n$/, "")}
+                style={dark}
+                language={match[1]}
+                PreTag="div"
+              />
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            );
+          },
+          // Map `h1` (`# heading`) to use `h2`s.
+          h1: "h2",
+          // Rewrite `em`s (`*like so*`) to `i` with a red foreground color.
+          em: ({ node, ...props }) => <i style={{ color: "red" }} {...props} />,
+        }}
+      >
+        {markdownContent}
+      </ReactMarkdown>
     </div>
   );
 }
