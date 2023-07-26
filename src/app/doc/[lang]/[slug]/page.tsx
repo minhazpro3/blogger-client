47ee549fff01923e78e1e25fa2a028c0416da05c
codergalib2005 <coderboygalif@gmail.com>
2023-07-26 23:02:28 +0600

    adding new feature code highlighter

--- src/app/doc/[lang]/[slug]/page.tsx
+++ src/app/doc/[lang]/[slug]/page.tsx
@@ -6,18 +6,22 @@ interface MetaTypes {
   description?: string;
   keywords?: string;
 }
-
+// Load single page content as string to use getPostBySlug query
 const getPageContent = async (slug: string) => {
   const { meta, content } = await getPostBySlug(slug);
+  if (!content) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
   return { meta, content };
 };
-
+// Generate Meta Data using document title
 export async function generateMetadata({ params }: { params: string }) {
   const { meta } = await getPageContent(
     (params as unknown as { slug: string }).slug
   );
-  console.log(meta);
-  return { title: (meta as any).title };
+  return { title: (meta.data as MetaTypes).title };
 }
 
 const Page = async ({ params }: { params: { slug: string } }) => {
@@ -25,8 +29,6 @@ const Page = async ({ params }: { params: { slug: string } }) => {
 
   return (
     <section className="py-24">
-      {/* <div className='container py-4 prose'>{content}</div> */}
-      {/* <MarkdownPreview markdownContent={content} /> */}
       <div className="prose">
         <MarkdownPreview markdownContent={content} />
       </div>
