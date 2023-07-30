f9daad2b6ed927316661e9fc78f82e5fda4630e5
minhazpro3 <minhazpro1@gamil.com>
2023-07-30 17:17:19 +0600

    styled review rating

--- app/course-review/layout.tsx
+++ app/course-review/layout.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { Inter, Poppins, Roboto } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 const roboto = Roboto({ subsets: ['latin'], weight: "400" })
-const CourseLayout = ({children}:{children:React.ReactNode}) => {
+const CourseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={roboto.className}>
-        <div>
+      <div>
 
-        </div>
-        <div>
-            {children}
-        </div>
+      </div>
+      <div>
+        {children}
+      </div>
     </div>
   )
 }
---
+++ src/components/course-review/ReviewRatingBanner.tsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { AiFillStar } from 'react-icons/ai'
+import ReactStars from 'react-rating-star-with-type'
+const ReviewRatingBanner = () => {
+    return (
+        <div>
+            <div className='text-white my-8'>
+                <h2 className='text-2xl my-4'>Student Ratings & Reviews!</h2>
+                <div className='border-2 border-gray-800 p-8 bg-gray-950 rounded-lg    '>
+                    <div className='flex items-center gap-x-4  w-full'>
+
+                        {/* total rating */}
+                        <div className='bg-gray-800 p-8 w-56 rounded-xl shadow-xl'>
+                            <div className='  flex flex-col gap-y-2 items-center'>
+                                <h1 className='text-blue-800 text-4xl font-semibold  '>4.8</h1>
+
+                                <ReactStars
+                                    // onChange={onChange} 
+                                    value={4.2}
+                                    // edit={true}  
+                                    activeColors={["yellow",]}
+                                />
+                                <p>Total Rating (3)</p>
+                            </div>
+                        </div>
+                        {/* review progress */}
+                        <div className='w-full flex flex-col gap-y-2'>
+                            <div className='flex flex-row gap-x-2 items-center'>
+                                <span>5</span>
+                                <AiFillStar size={20} fill='yellow' />
+                                <div className=" w-full h-[5px] bg-neutral-600">
+                                    <div
+                                        className="bg-yellow-400  p-0 h-[5px]   "
+                                        style={{ width: "90%" }}
+                                    >
+
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className='flex flex-row gap-x-2 items-center  '>
+                                <span>4</span>
+                                <AiFillStar size={20} fill='yellow' />
+                                <div className=" w-full h-[5px] bg-neutral-600">
+                                    <div
+                                        className="bg-yellow-400  p-0 h-[5px]   "
+                                        style={{ width: "70%" }}
+                                    >
+
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className='flex flex-row gap-x-2 items-center  '>
+                                <span>3</span>
+                                <AiFillStar size={20} fill='yellow' />
+                                <div className=" w-full h-[5px] bg-neutral-600">
+                                    <div
+                                        className="bg-yellow-400  p-0 h-[5px]   "
+                                        style={{ width: "60%" }}
+                                    >
+
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className='flex flex-row gap-x-2 items-center  '>
+                                <span>2</span>
+                                <AiFillStar size={20} fill='yellow' />
+                                <div className=" w-full h-[5px] bg-neutral-600">
+                                    <div
+                                        className="bg-yellow-400  p-0 h-[5px]   "
+                                        style={{ width: "50%" }}
+                                    >
+
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div className='flex flex-row gap-x-2 items-center  '>
+                                <span>1</span>
+                                <AiFillStar size={20} fill='yellow' />
+                                <div className=" w-full h-[5px] bg-neutral-600">
+                                    <div
+                                        className="bg-yellow-400  p-0 h-[5px]   "
+                                        style={{ width: "30%" }}
+                                    >
+
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ReviewRatingBanner
